ee6caf208b950ac6739d5ef867e8e2ad8ce775a4
mizanmahi <mizanmahi24@gmail.com>
2021-05-29 13:54:27 +0600

    adding file for gh-page

---
+++ src/components/footer.js
@@ -0,0 +1,12 @@
+const Footer = () => {
+   return (
+      <footer>
+         <p>
+            Made with ❤ by<a href="https://github.com/mizanmahi" target="blank">Mizanur Rahman</a>
+            
+         </p>
+      </footer>
+   );
+};
+
+export default Footer;
